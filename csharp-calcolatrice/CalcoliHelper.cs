5db6b4f14787199fc4b9923dde5c1e55f7f9caeb
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-24 09:33:00 +0200

    REFACTOR Functions = add arrow function

--- csharp-calcolatrice/CalcoliHelper.cs
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -10,65 +10,29 @@ namespace csharp_calcolatrice
     internal static class CalcoliHelper
     {
         // POLIMORFIZMO
-        public static int Somma(int a, int b)
-        {
-            return a + b;
-        }
+        public static int Somma(int a, int b) =>  return a + b;
 
-        public static double Somma(double a, double b)
-        {
-            return a + b;
-        }
+        public static double Somma(double a, double b) =>  return a + b;
 
-        public static int Sottrazione(int a, int b)
-        {
-            return a - b;
-        }
+        public static int Sottrazione(int a, int b) =>  return a - b;
 
-        public static double Sottrazione(double a, double b)
-        {
-            return a - b;
-        }
+        public static double Sottrazione(double a, double b) => return a - b;
 
-        public static int Moltiplicazione(int a, int b)
-        {
-            return a * b;
-        }
+        public static int Moltiplicazione(int a, int b) =>  return a * b;
 
-        public static double Moltiplicazione(double a, double b)
-        {
-            return a * b;
-        }
+        public static double Moltiplicazione(double a, double b) => return a* b;
 
-        public static int ValoreAssoluto(int a)
-        {
-            return a < 0 ? -a : a;
-        }
+        public static int ValoreAssoluto(int a) => return a < 0 ? -a : a;
 
-        public static double ValoreAssoluto(double a)
-        {
-            return a < 0 ? -a : a;
-        }
+        public static double ValoreAssoluto(double a) =>  return a < 0 ? -a : a;
 
-        public static int Min(int a, int b)
-        {
-            return a <= b ? a : b;
-        }
+        public static int Min(int a, int b) =>  return a <= b? a : b;
 
-        public static double Min(double a, double b)
-        {
-            return a <= b ? a : b;
-        }
+        public static double Min(double a, double b) => return a <= b? a : b;
 
-        public static int Max(int a, int b)
-        {
-            return a >= b ? a : b;
-        }
+        public static int Max(int a, int b) => return a >= b? a : b;
 
-        public static double Max(double a, double b)
-        {
-            return a >= b ? a : b;
-        }
+        public static double Max(double a, double b) => return a >= b? a : b;
 
         // BONUS
 
